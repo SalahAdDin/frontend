867ffc0c252cb02c3de10bce516bfa130f7710e1
José L. S. A <alagunasalahaddin@live.com>
2020-07-08 18:05:15 +0300

    :tada: adding missing comments.

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -18,6 +18,8 @@ export default async function preview(req, res) {
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({})
 
+  // Redirect to the path from the fetched post
+  // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
   res.writeHead(307, { Location: `/page/${page.slug}` })
   res.end()
 }
