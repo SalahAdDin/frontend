f8a450df10b70a7be7e24fd63724d5cea9ee9801
José L. S. A <alagunasalahaddin@live.com>
2020-07-14 14:34:24 +0300

    [2] - Fixing external link redirection.
    As they recommend here: https://stackoverflow.com/a/61061107/3826549

--- components/fields/url.js
+++ components/fields/url.js
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types"
+import Link from "next/link"
 import { ListItemIcon, ListItem } from "@material-ui/core"
 import { GitHub, InsertLink, LinkedIn } from "@material-ui/icons"
 import useStyles from "@/styles/common"
@@ -9,19 +10,21 @@ const URL = ({ link }) => {
   const { type, url } = link
   return (
     <ListItem className={classes.inlineBlock}>
-      <a href={url}>
-        <ListItemIcon>
-          {
+      <Link href={"//" + url}>
+        <a>
+          <ListItemIcon>
             {
-              Github: <GitHub />,
-              BitBucket: <InsertLink />,
-              GitLab: <InsertLink />,
-              StackOverflow: <InsertLink />,
-              LinkedIn: <LinkedIn />,
-            }[type]
-          }
-        </ListItemIcon>
-      </a>
+              {
+                Github: <GitHub />,
+                BitBucket: <InsertLink />,
+                GitLab: <InsertLink />,
+                StackOverflow: <InsertLink />,
+                LinkedIn: <LinkedIn />,
+              }[type]
+            }
+          </ListItemIcon>
+        </a>
+      </Link>
     </ListItem>
   )
 }
