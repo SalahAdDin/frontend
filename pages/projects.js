58b623c84febf0626725e454d7008e41e2e04fad
José L. S. A <alagunasalahaddin@live.com>
2020-07-22 13:37:14 +0300

    :tada: adding initial projects web site.

---
+++ pages/projects.js
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types"
+import { getPageBySlugAndCategory } from "@/lib/api"
+
+function Projects({ title_en, slug, title, description, body, categories }) {
+  return <div></div>
+}
+
+Projects.propTypes = {
+  title_en: PropTypes.string.isRequired,
+  title: PropTypes.objectOf(PropTypes.string),
+  description: PropTypes.objectOf(PropTypes.string).isRequired,
+  body: PropTypes.arrayOf(PropTypes.object).isRequired,
+  slug: PropTypes.string.isRequired,
+  categories: PropTypes.arrayOf(PropTypes.object),
+}
+
+export const getStaticProps = async () => {
+  const data = await getPageBySlugAndCategory("projects")
+  return { props: { ...data?.pages[0], categories: data?.categories } }
+}
+
+export default Projects
