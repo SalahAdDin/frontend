f75e40d0c23839c25733955752ba5565f8d14c3e
José L. S. A <alagunasalahaddin@live.com>
2020-07-22 13:41:40 +0300

    :tada: adding initial posts page.

---
+++ pages/posts.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types"
+import { getPageAndTagBySlug } from "@/lib/api"
+
+const Posts = ({ title_en, slug, title, description, body, pagesByTag }) => {
+  return <div></div>
+}
+
+Posts.propTypes = {
+  title_en: PropTypes.string.isRequired,
+  title: PropTypes.objectOf(PropTypes.string),
+  description: PropTypes.objectOf(PropTypes.string).isRequired,
+  body: PropTypes.arrayOf(PropTypes.object).isRequired,
+  slug: PropTypes.string.isRequired,
+  pagesByTag: PropTypes.arrayOf(PropTypes.object),
+}
+
+export const getStaticProps = async () => {
+  const data = await getPageAndTagBySlug("posts", "blog")
+  return {
+    props: { ...data?.pages[0], pagesByTag: data?.tags[0] },
+  }
+}
+
+export default Posts
