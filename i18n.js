f0d1961440bb7741f781b4ebc6d988bdf7f04d19
José L. S. A <alagunasalahaddin@live.com>
2020-07-24 20:23:01 +0300

    :fire: minor fixes.

--- i18n.js
+++ i18n.js
@@ -1,6 +1,6 @@
 const path = require("path")
 const NextI18Next = require("next-i18next").default
-const { localeSubpaths } = require("next/config").default().publicRuntimeConfig
+// const { localeSubpaths } = require("next/config").default().publicRuntimeConfig
 
 module.exports = new NextI18Next({
   otherLanguages: ["es", "tr"],
