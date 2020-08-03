3c4d1aabf682f0536236688e862842494001c073
José L. S. A <alagunasalahaddin@live.com>
2020-08-03 20:52:28 +0300

    :tada: adding a page for each tag.
    I'm getting this bug at clicking on tag:
    `page-loader.js?d172:271 GET http://localhost:3000/_next/static/development/pages/tags/reactjs.js net::ERR_ABORTED 404 (Not Found)`

---
+++ pages/tags/[slug].js
@@ -0,0 +1,77 @@
+import PropTypes from "prop-types"
+import ErrorPage from "next/error"
+import { useRouter } from "next/router"
+import { useTranslation } from "react-i18next"
+import { Skeleton } from "@material-ui/lab"
+import { getTagBySlug, getAllTagsWithSlug } from "@/lib/api/tags"
+import { getAllCategoriesWithSlug } from "@/lib/api/categories"
+import Layout from "@/components/layout"
+import { Typography, Grid } from "@material-ui/core"
+import Project from "@/components/project"
+import Post from "@/components/post"
+
+const Tag = ({ slug, label, projects, pages }) => {
+  const router = useRouter()
+  const { t } = useTranslation()
+
+  if (!router.isFallback && !slug) return <ErrorPage statusCode={404} />
+
+  return (
+    <Layout>
+      {router.isFallback ? (
+        <Skeleton />
+      ) : (
+        <>
+          {/* TODO: Title has a left/right padding */}
+          <Typography variant="h3" component="h3" gutterBottom>
+            {label}
+          </Typography>
+          {projects?.length > 0 && (
+            <>
+              <Typography variant="h5" component="h5" gutterBottom>
+                {t("projects")}
+              </Typography>
+              <Grid container component="section" spacing={2}>
+                {projects?.map((project) => (
+                  <Project key={"project_" + project.id} {...project} />
+                ))}
+              </Grid>
+            </>
+          )}
+          {pages?.length > 0 && (
+            <>
+              <Typography variant="h5" component="h5" gutterBottom>
+                {t("pages")}
+              </Typography>
+              <Grid container component="section" spacing={2}>
+                {pages?.map((page) => (
+                  <Post key={"page_" + page.id} {...page} />
+                ))}
+              </Grid>
+            </>
+          )}
+        </>
+      )}
+    </Layout>
+  )
+}
+
+Tag.propTypes = {
+  label: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+  projects: PropTypes.arrayOf(PropTypes.object),
+  pages: PropTypes.arrayOf(PropTypes.object),
+}
+
+export const getStaticProps = async ({ params }) => {
+  const data = await getTagBySlug(params.slug)
+  return { props: { ...data?.tags[0] }, unstable_revalidate: 1 }
+}
+
+export const getStaticPaths = async () => {
+  const tags = await getAllTagsWithSlug()
+
+  return { paths: tags?.map((tag) => `/tags/${tag.slug}`) || [], fallback: true }
+}
+
+export default Tag
