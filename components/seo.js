671beabf3259d1c3452bac655b4016a2b1c2a0e8
José L. S. A <alagunasalahaddin@live.com>
2020-08-09 16:09:16 +0300

    :small: Adding support for canonical url in SEO.

--- components/seo.js
+++ components/seo.js
@@ -3,7 +3,7 @@ import { NextSeo } from "next-seo"
 import { useTranslation } from "react-i18next"
 import { CMS_NAME } from "@/lib/constants"
 
-const SEO = ({ title_en, title = {}, openGraph, description = {} }) => {
+const SEO = ({ title_en, title = {}, openGraph, description = {}, canonical }) => {
   const { i18n } = useTranslation()
 
   const pageTitle = `${CMS_NAME} | ${
@@ -22,12 +22,10 @@ const SEO = ({ title_en, title = {}, openGraph, description = {} }) => {
     <NextSeo
       title={pageTitle}
       description={localDescription}
-      // TODO: get url
-      // canonical={process.env.BASE_URL}
-
+      canonical={canonical || process.env.NEXT_PUBLIC_BASE_URL}
       openGraph={{
-        // TODO: get the url => the the current path with next router
-        url: process.env.BASE_URL,
+        type: "website",
+        url: canonical || process.env.NEXT_PUBLIC_BASE_URL,
         title: pageTitle,
         description: localDescription,
         ...openGraph,
@@ -37,6 +35,7 @@ const SEO = ({ title_en, title = {}, openGraph, description = {} }) => {
 }
 
 SEO.propTypes = {
+  canonical: PropTypes.string,
   title_en: PropTypes.string.isRequired,
   title: PropTypes.objectOf(PropTypes.string),
   description: PropTypes.objectOf(PropTypes.string),
